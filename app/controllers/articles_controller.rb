7956e723817f65a46ab9c1c60c14f1be28443166
Swe Sin Tha <sweaway@gmail.com>
2019-09-19 17:25:55 +0800

    5.6 saving data in the controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,19 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        render plain: params[:article].inspect
+        #render plain: params[:article].inspect
+
+        #@article = Article.new(params[:article])
+
+        #@article = Article.new(params.require(:article).permit(:title, :text))
+        
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to @article
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
     end
 end
