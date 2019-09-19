f3ca6844fb6cb5b181cc877b23d1e679ae0ac907
Swe Sin Tha <sweaway@gmail.com>
2019-09-19 18:01:34 +0800

    5.8 Listing all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-    def new
+    def index
+        @articles = Article.all
     end
 
     def create
@@ -14,8 +15,14 @@ class ArticlesController < ApplicationController
         redirect_to @article
     end
 
+    def show
+        @article = Article.find(params[:id])
+    end
+
     private
     def article_params
         params.require(:article).permit(:title, :text)
     end
+
+    
 end
