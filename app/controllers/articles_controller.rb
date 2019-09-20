2640330ac7df1ded610e99148a6ea09fe3ff40b2
Swe Sin Tha <sweaway@gmail.com>
2019-09-20 16:05:49 +0800

    5.11

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,12 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
-
     def new
         @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
@@ -28,6 +31,16 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
     end
 
     private
